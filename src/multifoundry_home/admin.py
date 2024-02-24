cd82f5e23ca0c122bc396618d4e4e2b3064cb5e4
ForOhForError <16399039+ForOhForError@users.noreply.github.com>
2024-02-24 17:03:12 -0500

    making the models a bit more useful

--- src/multifoundry_home/admin.py
+++ src/multifoundry_home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from multifoundry_home.models.foundry_models import (
+    FoundryInstance, FoundryVersion, FoundryLicense
+)
+
+@admin.register(FoundryInstance, FoundryVersion, FoundryLicense)
+class MultifoundryAdmin(admin.ModelAdmin):
+    pass
--- src/multifoundry_home/models/foundry_models.py
+++ src/multifoundry_home/models/foundry_models.py
@@ -1,14 +1,35 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 class FoundryInstance(models.Model):
     instance_name = models.CharField(max_length=30, unique=True)
+    foundry_version = models.ForeignKey(
+        "FoundryVersion",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
     
 class FoundryVersion(models.Model):
+    class UpdateType(models.TextChoices):
+        FULL = "Full", _("Full")
+        UPDATE = "Update", _("Update")
+
+    class UpdateCategory(models.TextChoices):
+        PROTOTYPE = "Prototype", _("Prototype")
+        DEVELOPMENT = "Development", _("Development")
+        TESTING = "Testing", _("Testing")
+        STABLE = "Stable", _("Stable")
+
     version_string = models.CharField(max_length=30, unique=True)
+    update_type = models.CharField(max_length=10, choices=UpdateType.choices, default=UpdateType.FULL)
+    update_category = models.CharField(max_length=15, choices=UpdateCategory.choices, default=UpdateCategory.STABLE)
+    downloaded = models.BooleanField(default=False)
 
 FOUNDRY_LICENSE_REGEX = "^[A-Z1-9]{4}-[A-Z1-9]{4}-[A-Z1-9]{4}-[A-Z1-9]{4}-[A-Z1-9]{4}-[A-Z1-9]{4}$"
-foundry_license_validator = RegexValidator(FOUNDRY_LICENSE_REGEX, "Foundry Licenses are of format XXXX-XXXX-XXXX-XXXX-XXXX-XXXX")
+foundry_license_validator = RegexValidator(FOUNDRY_LICENSE_REGEX, _("Foundry Licenses are of format XXXX-XXXX-XXXX-XXXX-XXXX-XXXX"))
 
 class FoundryLicense(models.Model):
     license_key = models.CharField(max_length=29, validators=[foundry_license_validator])
